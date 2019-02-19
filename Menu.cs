589e70d6c045c353ee6936c6d0bff4ea00325daa
Mateusz <38114179+furiousmatthew@users.noreply.github.com>
2019-02-19 19:49:11 +0100

    ukończenie przełączania pozycji menu i zmiany ich tła na biało

--- Menu.cs
+++ Menu.cs
@@ -15,28 +15,113 @@ namespace SNAKE
 
         public void viewMenu ()
         {
-        
-                Console.WriteLine("Welcome in the game 'SNAKE' by Matt Poltorak");
-                Console.WriteLine("New Game");
-                Console.WriteLine("Options");
-                Console.WriteLine("Quit");
 
-                ConsoleKeyInfo cki;
+            string [] MenuOptions = new string[4];
+            MenuOptions[0] = "Welcome in the game 'SNAKE' by Matt Poltorak";
+            MenuOptions[1] = "New Game";
+            MenuOptions[2] = "Options";
+            MenuOptions[3] = "Quit";
+
+            int position = 1;
+            int status;
+            ConsoleKeyInfo cki;
+
+            do
+            {
+
+                switch (position)
+                    {
+                        case 1:
+                            {                       
+                                Console.WriteLine(MenuOptions[0]);
+                                Console.BackgroundColor = ConsoleColor.White;              
+                                Console.WriteLine(MenuOptions[1]);
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                Console.WriteLine(MenuOptions[2]);
+                                Console.WriteLine(MenuOptions[3]);
+
+                       
+                                break;
+                            }
+
+                        case 2:
+                            {
+                                Console.WriteLine(MenuOptions[0]);                           
+                                Console.WriteLine(MenuOptions[1]);
+                                Console.BackgroundColor = ConsoleColor.White;                            
+                                Console.WriteLine(MenuOptions[2]);
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                Console.WriteLine(MenuOptions[3]);
+
+                     
+
+                            break;
+                            }
+
+                        case 3:
+                            {
+                                Console.WriteLine(MenuOptions[0]);
+                                Console.WriteLine(MenuOptions[1]);
+                                Console.WriteLine(MenuOptions[2]);
+                                Console.BackgroundColor = ConsoleColor.White;
+                                Console.WriteLine(MenuOptions[3]);
+                                Console.BackgroundColor = ConsoleColor.Black;
+
+                        
+
+                            break;
+                            }
+                }
+
+              
+
+
                 cki = Console.ReadKey();
+                
 
                 switch (cki.Key)
                 {
+                   
 
-                case ConsoleKey.UpArrow:
-                    {
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (position == 1)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                position--;
+                                break;
+                            }
+                            
+                        }
 
-                    }
+                    case ConsoleKey.DownArrow:
+                        {
+                            if(position == 3)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                position++;
+                                break;
+                            }
+                            
+                        }
+
+                };
+
+                Console.Clear();
+
+
+            } while (cki.Key != ConsoleKey.Enter );
 
 
 
-               }
-            
-            
+
+
 
 
 
